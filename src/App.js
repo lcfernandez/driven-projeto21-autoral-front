c08263b14d068a4c9e2d309f721a32aded87079b
lcfernandez <lcfernandez@cos.ufrj.br>
2023-03-24 16:58:12 -0300

    feat: configure initial routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from "./assets/styles/GlobalStyle";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }

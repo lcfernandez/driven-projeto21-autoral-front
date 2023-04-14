fcbba751c8db67992adea538fee2b3304f411b65
lcfernandez <lcfernandez@cos.ufrj.br>
2023-04-14 02:23:40 -0300

    feat: implement lanes layout and actions

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,3 +1,5 @@
 export const BLACK_COLOR = "#000000";
 export const GREY_COLOR = "#555555";
+export const OFF_WHITE_COLOR = "#FAF9F6";
+export const SILVER_COLOR = "#C0C0C0";
 export const WHITE_COLOR = "#FFFFFF";
--- src/pages/ProjectPage/ProjectPage.js
+++ src/pages/ProjectPage/ProjectPage.js
@@ -1,22 +1,24 @@
 import { UserContext } from "../../contexts/UserContext";
 import { HeaderApp } from "../../components/HeaderApp";
-import { GREY_COLOR, WHITE_COLOR } from "../../constants/color";
+import { GREY_COLOR, OFF_WHITE_COLOR, SILVER_COLOR, WHITE_COLOR } from "../../constants/color";
+import { TEXT_FONT } from "../../constants/fonts";
 import { AddCircleButton, Title } from "../ProjectsPage/ProjectsPage";
 import { ThreeDots } from "react-loader-spinner";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Trash } from "grommet-icons";
+import { FormEdit, FormClose, FormAdd } from "grommet-icons";
 import styled from "styled-components";
 import axios from "axios";
-import { TEXT_FONT } from "../../constants/fonts";
 
 export function ProjectPage() {
   const [token] = useContext(UserContext);
 
   const [images, setImages] = useState(undefined);
+  const [lanes, setLanes] = useState(undefined);
   const [moodboardId, setMoodboardId] = useState(undefined);
   const [projectName, setProjectName] = useState("");
   const [updateImages, setUpdateImages] = useState(false);
+  const [updateLanes, setUpdateLanes] = useState(false);
 
   const navigate = useNavigate();
 
@@ -41,6 +43,49 @@ export function ProjectPage() {
         );
     }
   }
+
+  function deleteLane(id, title) {
+    if (window.confirm(`Deseja mesmo excluir a lista ${title}?`)) {
+      const header = { headers: { "Authorization": `Bearer ${token}` } };
+
+      axios
+        .delete(`${process.env.REACT_APP_API_URI}/lanes/${id}`, header)
+        .then(
+          () => {
+            alert("Lista excluída com sucesso!")
+            setUpdateLanes(!updateLanes);
+          }
+        )
+        .catch(
+          err => {
+            alert(err.response.data.message || err.response.data);
+          }
+        );
+    }
+  }
+
+  function editLane(laneId, title) {
+    const newTitle = prompt("Qual será o novo título da lista?", title);
+    
+    if (newTitle && newTitle !== title) {
+      const header = { headers: { "Authorization": `Bearer ${token}` } };
+      const body = { title: newTitle, project_id: Number(id) };
+
+      axios
+        .put(`${process.env.REACT_APP_API_URI}/lanes/${laneId}`, body, header)
+        .then(
+          () => {
+            alert("Lista renomeada com sucesso!")
+            setUpdateLanes(!updateLanes);
+          }
+        )
+        .catch(
+          err => {
+            alert(err.response.data.message || err.response.data);
+          }
+        );
+    }
+  }
   
   function handleImages() {
     if (images.length === 0) {
@@ -52,12 +97,48 @@ export function ProjectPage() {
         image => <li key={image.id}>
           <ImgContainer>
             <ImgStyled alt={`Imagem ${image.id}`} src={image.url} />
-            <TrashAction
+            <FormCloseAction
               //disabled={disabled && true}
               onClick={() => deleteImage(image.id)}
               size="18px"
             />
           </ImgContainer>
+        </li>
+      )
+    );
+  }
+
+  function handleLanes() {
+    if (lanes.length === 0) {
+      return <h3>Você ainda não tem listas criadas</h3>;
+    }
+
+    return (
+      lanes.map(
+        lane => <li key={lane.id}>
+          <Lane>
+            <TitleLane>
+              {lane.title}
+            </TitleLane>
+            
+            <span>
+              <FormAdd
+                //disabled={disabled && true}
+                //onClick={() => editLane(lane.id, lane.title)}
+                size="22px"
+              />
+              <FormEdit
+                //disabled={disabled && true}
+                onClick={() => editLane(lane.id, lane.title)}
+                size="22px"
+              />
+              <FormClose
+                //disabled={disabled && true}
+                onClick={() => deleteLane(lane.id, lane.title)}
+                size="22px"
+              />
+            </span>
+          </Lane>
         </li>
       )
     );
@@ -86,19 +167,19 @@ export function ProjectPage() {
     }
   }
 
-  useEffect(() => {
-    if (!token) {
-      navigate("/");
-    } else {
+  function insertLane() {
+    const title = prompt("Qual será o nome da lista?");
+
+    if (title) {
       const header = { headers: { "Authorization": `Bearer ${token}` } };
+      const body = { title, project_id: Number(id) };
 
       axios
-        .get(`${process.env.REACT_APP_API_URI}/projects/${id}/moodboard`, header)
+        .post(`${process.env.REACT_APP_API_URI}/lanes`, body, header)
         .then(
-          res => {
-            setImages(res.data.images);
-            setProjectName(res.data.project_name);
-            setMoodboardId(res.data.moodboard_id);
+          () => {
+            alert("Lista criada com sucesso!")
+            setUpdateLanes(!updateLanes);
           }
         )
         .catch(
@@ -107,7 +188,59 @@ export function ProjectPage() {
           }
         );
     }
-  }, [navigate, token, updateImages, id]);
+  }
+
+  function reLoadImages() {
+    const header = { headers: { "Authorization": `Bearer ${token}` } };
+
+    axios
+      .get(`${process.env.REACT_APP_API_URI}/projects/${id}/moodboard`, header)
+      .then(
+        res => {
+          setImages(res.data.images);
+          setProjectName(res.data.project_name);
+          setMoodboardId(res.data.moodboard_id);
+        }
+      )
+      .catch(
+        err => {
+          alert(err.response.data.message || err.response.data);
+        }
+      );
+  };
+
+  function reLoadLanes() {
+    const header = { headers: { "Authorization": `Bearer ${token}` } };
+
+    axios
+      .get(`${process.env.REACT_APP_API_URI}/projects/${id}/lanes`, header)
+      .then(
+        res => {
+          setLanes(res.data);
+        }
+      )
+      .catch(
+        err => {
+          alert(err.response.data.message || err.response.data);
+        }
+      );
+  };
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+    } else {
+      reLoadImages();
+    }
+  }, [updateImages]);
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+    } else {
+      reLoadLanes();
+    }
+  }, [updateLanes]);
 
   return (
     <>
@@ -140,36 +273,98 @@ export function ProjectPage() {
             }
           </ul>
         </MoodboardContainer>
+
+        <LanesContainer>
+          <Title>
+            <h1>Listas</h1>
+            <AddCircleButton
+              color={GREY_COLOR}
+              //disabled={disabled && true}
+              onClick={insertLane}
+            />
+          </Title>
+
+          <ul>
+            {
+              lanes ? handleLanes() :
+                <ThreeDots
+                  ariaLabel="three-dots-loading"
+                  color={GREY_COLOR}
+                  height={50}
+                  width={50}
+                />
+            }
+          </ul>
+        </LanesContainer>
       </ProjectPageContainer>
     </>
   );
 }
+
+const FormCloseAction = styled(FormClose)`
+  background-color: ${OFF_WHITE_COLOR};
+  cursor: pointer;
+  position: absolute;
+  right: 0;
+`;
 
 const ImgContainer = styled.div`
   position: relative;
 `;
 
 const ImgStyled = styled.img`
-  height: 15rem;
-  max-width: 20.6rem;
+  height: auto;
+  max-height: 15rem;
+  max-width: calc(50vw - 1.25rem);
+  width: auto;
+`;
+
+const LanesContainer = styled.div`
+  height: calc(100vh - 8.5rem);
+  overflow: scroll;
+  width: calc(50vw - 1rem);
+
+  ul {
+    display: flex;
+    margin-top: 2rem;
+  }
+
+  @media (max-width: 600px) {
+    width: calc(100vw - 2rem);
+  }
+`;
+
+const Lane = styled.div`
+  background-color: ${SILVER_COLOR};
+  border-radius: 0.5rem;
+  display: flex;
+  height: 30rem;
+  justify-content: space-between;
+  margin-right: 0.5rem;
+  width: 20rem;
+
+  span {
+    * {
+      cursor: pointer;
+      margin: 0.25rem;
+    }
+  }
 `;
 
 const MoodboardContainer = styled.div`
-`;
-
-const ProjectPageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: calc(100vh - 5rem);
-  padding: 1rem;
+  overflow: scroll;
+  width: calc(50vw - 1rem);
 
   li {
+    background-color: ${OFF_WHITE_COLOR};
     display: flex;
-    margin-bottom: 1rem;
+    margin-bottom: 0.5rem;
+    padding: 0.1rem;
   }
 
   ul {
     display: flex;
+    flex-wrap: wrap;
     margin: 2rem 0;
 
     a {
@@ -178,14 +373,35 @@ const ProjectPageContainer = styled.div`
     }
 
     @media (max-width: 600px) {
-      display: block;
+      justify-content: center;
       text-align: center;
     }
   }
+
+  @media (max-width: 600px) {
+    width: calc(100vw - 2rem);
+  }
+`;
+
+const ProjectPageContainer = styled.div`
+  display: flex;
+  height: calc(100vh - 7.5rem);
+  padding: 1rem;
+
+  @media (max-width: 600px) {
+    display: block;
+  }
+`;
+
+const TitleLane = styled.div`
+  color: ${GREY_COLOR};
+  font-family: ${TEXT_FONT};
+  font-size: 1rem;
+  padding: 0.5rem;;
 `;
 
 const TitleProject = styled.div`
-  background-color: ${GREY_COLOR};
+  background-color: ${SILVER_COLOR};
   color: ${WHITE_COLOR};
   font-family: ${TEXT_FONT};
   font-size: 1.5rem;
@@ -195,13 +411,4 @@ const TitleProject = styled.div`
     font-size: 1rem;
     text-align: center;
   }
-`;
-
-const TrashAction = styled(Trash)`
-  background-color: ${WHITE_COLOR};
-  cursor: pointer;
-  margin: 0.2rem;
-  padding: 0.055rem;
-  position: absolute;
-  right: 0;
 `;
